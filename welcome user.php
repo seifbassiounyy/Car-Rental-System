5e47888ad561d23f1539cb2d07e833c6b98252f5
Ali Ali Mohamed <81182273+jester487@users.noreply.github.com>
2022-12-20 22:50:08 +0200

    Add files via upload

---
+++ welcome user.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link href="Welcome user.css" rel="stylesheet">
+	<link rel="icon" href="https://cdn-icons-png.flaticon.com/512/809/809998.png" type="image/x-icon">
+	<title>Car Rental</title>
+	<script src="https://kit.fontawesome.com/77fd9664d1.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<nav class ="navbar">
+		<div class="logo"><h1>Car Rental <i class="fas fa-car"></i></h1></div>
+		<ul class="menu">
+			<li><a href="#">Home</a></li>
+			<li><a href="">Account</a></li>
+		</ul>
+		<div class="select">
+			<select name="format" id="format">
+				<option selected disabled>Select your country</option>
+				<option value="egy">Egypt</option>
+				<option value="usa">United States</option>
+				<option value="ksa">Saudie Arabia</option>
+			</select>
+		</div>
+	</nav>
+	
+	<section class="content">
+		<div class="welcome"><p>Welcome Back Ali</p></div>
+		<h1>Rent the best cars in the city now!!</h1>
+		<p>Rent the suitable car for you now with the best price in the city.</p>
+		<a href="#move"><button>Rent Now</button></a>
+	</section>
+
+	<h1 class="pheading" id="move">Availabe cars in your country</h1>
+
+	<section class="sec">
+		<div class="car">
+
+			<div class="card">
+				<div class="img"><img src="https://img-ik.cars.co.za/news-site-za/images/2022/06/a45-dyn.jpg?tr=h-347%2Cw-617" alt=""></div>
+				<div class="desc">Mercedes-Benz</div>
+				<div class="title">A-class</div>
+				<div class="box">
+					<div class="price">$500 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://images.prismic.io/carwow/c7c18949-7a17-4ec4-bc34-d0365c2027e7_2022+Audi+A3+Sportback+Front+3Q+Moving.jpg?fit=crop&q=60&w=1125&cs=tinysrgb&auto=format" alt=""></div>
+				<div class="desc">Audi</div>
+				<div class="title">A3</div>
+				<div class="box">
+					<div class="price">$400 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Nissan_Ariya%2C_left_front%2C_Nissan_Gallery_HQ%2C_2021.jpg/1920px-Nissan_Ariya%2C_left_front%2C_Nissan_Gallery_HQ%2C_2021.jpg" alt=""></div>
+				<div class="desc">Nissan</div>
+				<div class="title">Ariya</div>
+				<div class="box">
+					<div class="price">$350 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://www.motortrend.com/uploads/2022/02/2022_Toyota_Avalon_Touring_001.jpg?fit=around%7C875:492.1875" alt=""></div>
+				<div class="desc">Toyota</div>
+				<div class="title">Avalon</div>
+				<div class="box">
+					<div class="price">$500 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://img-ik.cars.co.za/news-site-za/images/2022/06/a45-dyn.jpg?tr=h-347%2Cw-617" alt=""></div>
+				<div class="desc">Mercedes-Benz</div>
+				<div class="title">A-class</div>
+				<div class="box">
+					<div class="price">$500 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://images.prismic.io/carwow/c7c18949-7a17-4ec4-bc34-d0365c2027e7_2022+Audi+A3+Sportback+Front+3Q+Moving.jpg?fit=crop&q=60&w=1125&cs=tinysrgb&auto=format" alt=""></div>
+				<div class="desc">Audi</div>
+				<div class="title">A3</div>
+				<div class="box">
+					<div class="price">$400 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Nissan_Ariya%2C_left_front%2C_Nissan_Gallery_HQ%2C_2021.jpg/1920px-Nissan_Ariya%2C_left_front%2C_Nissan_Gallery_HQ%2C_2021.jpg" alt=""></div>
+				<div class="desc">Nissan</div>
+				<div class="title">Ariya</div>
+				<div class="box">
+					<div class="price">$350 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://www.motortrend.com/uploads/2022/02/2022_Toyota_Avalon_Touring_001.jpg?fit=around%7C875:492.1875" alt=""></div>
+				<div class="desc">Toyota</div>
+				<div class="title">Avalon</div>
+				<div class="box">
+					<div class="price">$500 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://img-ik.cars.co.za/news-site-za/images/2022/06/a45-dyn.jpg?tr=h-347%2Cw-617" alt=""></div>
+				<div class="desc">Mercedes-Benz</div>
+				<div class="title">A-class</div>
+				<div class="box">
+					<div class="price">$500 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://images.prismic.io/carwow/c7c18949-7a17-4ec4-bc34-d0365c2027e7_2022+Audi+A3+Sportback+Front+3Q+Moving.jpg?fit=crop&q=60&w=1125&cs=tinysrgb&auto=format" alt=""></div>
+				<div class="desc">Audi</div>
+				<div class="title">A3</div>
+				<div class="box">
+					<div class="price">$400 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Nissan_Ariya%2C_left_front%2C_Nissan_Gallery_HQ%2C_2021.jpg/1920px-Nissan_Ariya%2C_left_front%2C_Nissan_Gallery_HQ%2C_2021.jpg" alt=""></div>
+				<div class="desc">Nissan</div>
+				<div class="title">Ariya</div>
+				<div class="box">
+					<div class="price">$350 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://www.motortrend.com/uploads/2022/02/2022_Toyota_Avalon_Touring_001.jpg?fit=around%7C875:492.1875" alt=""></div>
+				<div class="desc">Toyota</div>
+				<div class="title">Avalon</div>
+				<div class="box">
+					<div class="price">$500 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://img-ik.cars.co.za/news-site-za/images/2022/06/a45-dyn.jpg?tr=h-347%2Cw-617" alt=""></div>
+				<div class="desc">Mercedes-Benz</div>
+				<div class="title">A-class</div>
+				<div class="box">
+					<div class="price">$500 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://images.prismic.io/carwow/c7c18949-7a17-4ec4-bc34-d0365c2027e7_2022+Audi+A3+Sportback+Front+3Q+Moving.jpg?fit=crop&q=60&w=1125&cs=tinysrgb&auto=format" alt=""></div>
+				<div class="desc">Audi</div>
+				<div class="title">A3</div>
+				<div class="box">
+					<div class="price">$400 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Nissan_Ariya%2C_left_front%2C_Nissan_Gallery_HQ%2C_2021.jpg/1920px-Nissan_Ariya%2C_left_front%2C_Nissan_Gallery_HQ%2C_2021.jpg" alt=""></div>
+				<div class="desc">Nissan</div>
+				<div class="title">Ariya</div>
+				<div class="box">
+					<div class="price">$350 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="img"><img src="https://www.motortrend.com/uploads/2022/02/2022_Toyota_Avalon_Touring_001.jpg?fit=around%7C875:492.1875" alt=""></div>
+				<div class="desc">Toyota</div>
+				<div class="title">Avalon</div>
+				<div class="box">
+					<div class="price">$500 per day</div>
+					<button class="btn">Rent Now</button>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<footer>
+		<p>Contact us <a href="">Here</a></p>
+	</footer>
+
+<?php
+	
+
+?>
+</body>
+</html>
