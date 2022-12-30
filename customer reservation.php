8e7e7d09eade18d437d30f5243c1885e3f0efc95
Ahmed Sakr <asakr1191@gmail.com>
2022-12-31 01:22:51 +0200

    Add files via upload

--- customer reservation.php
+++ customer reservation.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-	<link href="customer reservation.css" rel="stylesheet">
+	<link href="carReservations.css" rel="stylesheet">
 	<link rel="icon" href="https://cdn-icons-png.flaticon.com/512/809/809998.png" type="image/x-icon">
 	<title>Car Rental - Customer Reservations</title>
 	<script src="https://kit.fontawesome.com/77fd9664d1.js" crossorigin="anonymous"></script>
@@ -13,6 +13,29 @@
 	include_once 'C:\xampp\htdocs\CarRentalSystem\once.php';
 	session_start();
 ?>
+
+<script>
+	function validateForm(){
+		var from= document.getElementById("from").value;
+		var to= document.getElementById("to").value;
+		if(from <= to)
+		{
+			if(from==document.getElementById("from").defaultValue){
+				alert("You must enter a date");
+				return false;
+			}
+			else if(to==document.getElementById("to").defaultValue){
+				alert("You must enter a date");
+				return false;
+			}
+		}
+		else
+		{
+			alert("You entred a WRONG date");
+			return false;
+		}
+	}
+</script>
 
 <body>
 	<nav class ="navbar">
@@ -24,9 +47,39 @@
 	</nav>
 
 	<h1> Customer Reservations </h1>
-    
+    <div class="specs">
+		<form id="period" name="period" action="customer reservation2.php" method="post" onsubmit="return validateForm()">
+			<div>
+				<label class="sort">Date From:</label><br>
+				<input type="date" class="form-control" name="from" id="from"/>
+			</div>
+			<div>
+				<br>
+				<label class="sort">To Date:</label><br>
+				<input type="date" class="form-control" name="to" id="to"/>
+			</div>
+            <div>
+                <br>
+                <label class="sort">Customer ID:</label> 
+                <input type="text" required name="customer_id"/>
+            </div>
+			<div>
+			<button class="btn" name="customer" type="submit"> Confirm </button>
+			</div>
+		</form>
+	</div>
 	<footer>
 		<p><a href="">Contact us</a></p>
 	</footer>
 </body>
+
+<?php
+	if(isset($_POST['period']))
+	{
+		$from = $_POST['from'];
+		$to = $_POST['to'];
+		$customer_id = $_POST['customer_id'];
+	}
+?>
+
 </html>
