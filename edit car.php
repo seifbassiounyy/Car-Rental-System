89eda61e339f9adf67d35a306822b36364b399e4
Ali Ali Mohamed <81182273+jester487@users.noreply.github.com>
2022-12-31 05:08:11 +0200

    Final

--- edit car.php
+++ edit car.php
@@ -9,32 +9,6 @@
 	<script src="https://kit.fontawesome.com/77fd9664d1.js" crossorigin="anonymous"></script>
 </head>
 
-<script>
-	function validateForm()
-	{
-		var from= document.getElementById("from").value;
-		var to= document.getElementById("to").value;
-		if(from!=document.getElementById("from").defaultValue && to==document.getElementById("to").defaultValue)
-		{
-			alert("You must enter an end date");
-			return false;
-		}
-		if(from==document.getElementById("from").defaultValue && to!=document.getElementById("to").defaultValue)
-		{
-			alert("You must enter a start date");
-			return false;
-		}
-		if(from!=document.getElementById("from").defaultValue && to!=document.getElementById("to").defaultValue)
-		{
-			if(from >= to)
-			{
-				alert("Start date MUST be before end date");
-				return false;
-			}
-		}
-	}
-</script>
-
 <?php
 	include_once 'C:\xampp\htdocs\CarRentalSystem\once.php';
 	session_start();
@@ -53,42 +27,11 @@
 		$brand = $_POST["brand"];
 		$model = $_POST["model"];
 		$price = $_POST["price"];
+		$status = $_POST["status"];
 		$year = $_POST["year"];
 		$image = $_POST["image"];
-		$from = $_POST["from"];
-		$to = $_POST["to"];
-		if($from != $to)
-		{
-			$temp = $from;
-			while($temp <= $to)
-			{
-				$query = "SELECT * FROM car_status WHERE car_status.Date='$temp'";
-				$result = mysqli_query($connect, $query);
-				if (mysqli_num_rows($result) > 0)
-				{
-					$update = $connect->prepare("UPDATE car_status SET `STATUS`='OutOfService' WHERE plate_id='$plate' AND `Date`='$temp'");
-					$update->execute();
-				}
-				else
-				{
-					$sql = "INSERT INTO car_status (Plate_id, `Date` ,`STATUS`) VALUES ('$plate','$temp','OutOfService')";
-        			mysqli_query($connect, $sql);
-				}
-				$query = "SELECT * FROM reservation WHERE reservation.Start_date<='$temp' AND reservation.End_date>='$temp' AND (reservation.`action`='Approved' OR reservation.`action`='Paid')";
-				$result = mysqli_query($connect, $query);
-				if (mysqli_num_rows($result) > 0)
-				{
-					$update = $connect->prepare("UPDATE reservation SET `Action`='RejectedLate' WHERE plate_id='$plate'
-					 							AND Start_date <='$temp' AND End_date>='$temp' AND
-												(`action`='Approved' OR `action`='Paid')");
-					$update->execute();
-				}
-				$temp = strtotime("+1 day", strtotime("$temp"));
-				$temp = date("Y-m-d",$temp);
-			}
-		}
 
-		$temp = $connect->prepare("UPDATE car SET Color='$color',Brand='$brand',Model='$model',Price_per_day='$price',car.Year='$year',car.Image='$image' WHERE Plate_id='$plate'");
+		$temp = $connect->prepare("UPDATE car SET Color='$color',Brand='$brand',Model='$model',Price_per_day='$price',car.Status='$status',car.Year='$year',car.Image='$image' WHERE Plate_id='$plate'");
 		$temp->execute();
 		header('location:welcome admin.php');
       }
@@ -142,23 +85,9 @@
 					<p>Image url:</p>
 					<input type="text" class="form-control" name="image" id="image" value="<?= $image ?>"/>
 				</div>
-				<div class="info">
-					<p>Out of Service from:</p>
-					<input type="date" class="form-control" name="from" id="from">
-				</div>
-				<div class="info">
-					<p>Out of Service to:</p>
-					<input type="date" class="form-control" name="to" id="to">
-				</div>
 			</div>
 
 			<div>
-</br>
-</br>
-</br>
-</br>
-</br>
-</br>
 				<button type="submit" class="btn btn-larger btn-block" value='submit1' name="submit1">Save</button>
 				<button type="submit" class="btn btn-larger btn-block" value='submit2' name="submit2">Delete</button>
 			</div>
