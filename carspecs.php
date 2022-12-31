43ea7a52d0e9a139c24da74da95dc2a6f0d10bae
Ali Ali Mohamed <81182273+jester487@users.noreply.github.com>
2022-12-31 04:59:18 +0200

    Final

--- carspecs.php
+++ carspecs.php
@@ -110,196 +110,207 @@ $currentDate = date("Y-m-d");
 	</nav>
 
 	<h1> Car Specs</h1>
-
+	<section class="content">
 	<div class="card">
 		<div class="img"><img src= <?= $picture ?> alt=""></div>
 	</div>
-		<div class="specs">
-			<div class="ans"><span class="sort">Brand: </span> <?= $Brand ?></div><br>
-			<div class="ans"><span class="sort">Model: </span> <?= $Model ?></div><br>
-			<div class="ans"><span class="sort">Year: </span> <?= $Year ?></div><br>
-			<div class="ans"><span class="sort">Color: </span> <?= $color ?></div><br>
-			<div class="ans"><span class="sort">Price/Day: </span> <?= $Price_per_day ?></div><br>
-			<div class="ans"><span class="sort">Status: </span> <?= $status ?></div><br>		
-			<div class="ans"><span class="sort">Office Name: </span> <?= $office ?></div><br>	
-			<div class="ans"><span class="sort">Office ID: </span> <?= $officeid ?></div><br>	
-			<div class="ans"><span class="sort">Plate Number: </span> <?= $plateid ?></div><br>
-			
-		</div>
-	<div>
+	
+			<div class="specs">
+				<div class="ans"><span class="sort">Brand: </span> <?= $Brand ?></div><br>
+				<div class="ans"><span class="sort">Model: </span> <?= $Model ?></div><br>
+				<div class="ans"><span class="sort">Year: </span> <?= $Year ?></div><br>
+				<div class="ans"><span class="sort">Color: </span> <?= $color ?></div><br>
+				<div class="ans"><span class="sort">Price/Day: </span> <?= $Price_per_day ?></div><br>
+				<div class="ans"><span class="sort">Status: </span> <?= $status ?></div><br>		
+				<div class="ans"><span class="sort">Office Name: </span> <?= $office ?></div><br>	
+				<div class="ans"><span class="sort">Office ID: </span> <?= $officeid ?></div><br>	
+				<div class="ans"><span class="sort">Plate Number: </span> <?= $plateid ?></div><br>
+				
+			</div>
+		<div>
 
-		<?php
-			$query=mysqli_query($connect,"SELECT national_id FROM customer WHERE Email='$email'");
-			$fetch = mysqli_fetch_assoc($query);
-			$nationalID = $fetch['national_id'];
-			
-			$query=mysqli_query($connect,"SELECT car_status.STATUS FROM car_status WHERE Plate_id='$plateid' AND car_status.Date='$currentDate'");
-			$fetch = mysqli_fetch_assoc($query);
-			if(isset($fetch['STATUS']) == 0)
-			{
-				$checkStatus = 'Active';
-			}
-			else
-			{
-				$checkStatus = $fetch['STATUS'];
-			}
-
-			if($checkStatus=='Active')
-			{
-				$query=mysqli_query($connect,"SELECT * FROM reservation WHERE Plate_id='$plateid' AND Reservation_id = (SELECT MAX(Reservation_id) FROM reservation Where Plate_id='$plateid')");
+			<?php
+				$query=mysqli_query($connect,"SELECT national_id FROM customer WHERE Email='$email'");
 				$fetch = mysqli_fetch_assoc($query);
-				if(isset($fetch['Action']) == 0)
+				$nationalID = $fetch['national_id'];
+				
+				$query=mysqli_query($connect,"SELECT car_status.STATUS FROM car_status WHERE Plate_id='$plateid' AND car_status.Date='$currentDate'");
+				$fetch = mysqli_fetch_assoc($query);
+				if(isset($fetch['STATUS']) == 0)
 				{
-					$checkStatus = 'Active without reservation';
+					$checkStatus = 'Active';
 				}
 				else
 				{
-					$checkStatus = $fetch['Action'];
+					$checkStatus = $fetch['STATUS'];
 				}
 
-				if($checkStatus == 'Rejected' || $checkStatus == 'Paid')
+				if($checkStatus=='Active')
 				{
-					?>
-						<form method='POST' action="" onsubmit="return validateForm()">
-						<div class="info">
-							<label class="sort">Pickup Date:</label><br>
-							<input type="date" class="form-control" name="pickupDate" id="pickupDate"/>
-						</div>
-						<br>
-						<div class="info">
-							<label class="sort">Return Date:</label><br>
-							<input type="date" class="form-control" name="returnDate" id="returnDate"/>
-						</div>
-						<div><button class="btn3" type="button" onclick="calculatePrice(<?= $Price_per_day ?>)"><span>Calculate Total Price</span></button><br>
-							<label class="sort">Total Price:</label><br>
-							<p class="ans">$<p class="ans" id="output"></p></p>
-						</div>
-							<button class="btn2" name=reserve type="submit"><span>Reserve</span></button>
-						</form>
-						<form method="get" name="form" action="">
-							<button class="btn1" name="plate2" type="submit" disabled><span> Pay </span></button>
-						</form>
-					<?php
-				}
-				elseif($checkStatus == 'Approved')
-				{
-					if($fetch['national_id'] == $nationalID)
+					$query=mysqli_query($connect,"SELECT * FROM reservation WHERE Plate_id='$plateid' AND Reservation_id = (SELECT MAX(Reservation_id) FROM reservation Where Plate_id='$plateid')");
+					$fetch = mysqli_fetch_assoc($query);
+					if(isset($fetch['Action']) == 0)
+					{
+						$checkStatus = 'Active without reservation';
+					}
+					else
+					{
+						$checkStatus = $fetch['Action'];
+					}
+
+					if($checkStatus == 'Rejected' || $checkStatus == 'Paid')
 					{
 						?>
-						<form method='POST' action="" onsubmit="return validateForm()">
-						<div class="info">
-							<label class="sort">Pickup Date:</label><br>
-							<input type="date" class="form-control" name="pickupDate" id="pickupDate"/>
+						<div class="formDate">
+							<form method='POST' action="" onsubmit="return validateForm()">
+							<div class="info">
+								<label class="sort">Pickup Date:</label><br>
+								<input type="date" class="form-control" name="pickupDate" id="pickupDate"/>
+							</div>
+							<br>
+							<div class="info">
+								<label class="sort">Return Date:</label><br>
+								<input type="date" class="form-control" name="returnDate" id="returnDate"/>
+							</div>
+							<div><button class="btn3" type="button" onclick="calculatePrice(<?= $Price_per_day ?>)"><span>Calculate Total Price</span></button><br>
+								<label class="sort">Total Price:</label><br>
+								<p class="ans">$<p class="ans" id="output"></p></p>
+							</div>
+								<button class="btn2" name=reserve type="submit"><span>Reserve</span></button>
+							</form>
+							<form method="get" name="form" action="">
+								<button class="btn1" name="plate2" type="submit" disabled><span> Pay </span></button>
+							</form>
 						</div>
-						<br>
-						<div class="info">
-							<label class="sort">Return Date:</label><br>
-							<input type="date" class="form-control" name="returnDate" id="returnDate"/>
+						<?php
+					}
+					elseif($checkStatus == 'Approved')
+					{
+						if($fetch['national_id'] == $nationalID)
+						{
+							?>
+							<div class="formDate">
+							<form method='POST' action="" onsubmit="return validateForm()">
+							<div class="info">
+								<label class="sort">Pickup Date:</label><br>
+								<input type="date" class="form-control" name="pickupDate" id="pickupDate"/>
+							</div>
+							<br>
+							<div class="info">
+								<label class="sort">Return Date:</label><br>
+								<input type="date" class="form-control" name="returnDate" id="returnDate"/>
+							</div>
+							<div><button class="btn3" type="button" onclick="calculatePrice(<?= $Price_per_day ?>)"><span>Calculate Total Price</span></button><br>
+								<label class="sort">Total Price:</label><br>
+								<p class="ans"><span style="color:green">$</span><p class="ans" id="output"></p></p>
+							</div>
+								<button class="btn1" name=reserve type="submit" disabled><span>Reserve</span></button>
+							</form>
+							<form method="get" name="form" action="checkout.php">
+								<button class="btn" name="plate2" type="submit" value=<?= $plateid ?>><span> Pay </span></button>
+							</form>
+							</div>
+							<?php
+						}
+						else
+						{
+							?>
+							<div class="formDate">
+							<form method='POST' action='' onsubmit="return validateForm()">
+							<div class="info">
+								<label class="sort">Pickup Date:</label><br>
+								<input type="date" class="form-control" name="pickupDate" id="pickupDate"/>
+							</div>
+							<br>
+							<div class="info">
+								<label class="sort">Return Date:</label><br>
+								<input type="date" class="form-control" name="returnDate" id="returnDate"/>
+							</div>
+							<div><button class="btn3" type="button" onclick="calculatePrice(<?= $Price_per_day ?>)"><span>Calculate Total Price</span></button>
+								<label class="sort">Total Price:</label>
+								<p class="ans"><span style="color:green">$</span><p class="ans" id="output"></p></p>
+							</div>
+								<button class="btn1" name=reserve type="submit" disabled><span>Reserve</span></button>
+							</form>
+							<form method="get" name="form" action="checkout.php">
+								<button class="btn1" name="plate2" type="submit" disabled><span> Pay </span></button>
+							</form>
+							</div>
+							<?php
+						}
+					}
+					elseif($checkStatus == 'Pending')
+					{
+						?>
+						<div class="formDate">
+							<form method='POST' action="" onsubmit="return validateForm()">
+							<div class="info">
+								<label class="sort">Pickup Date:</label><br>
+								<input type="date" class="form-control" name="pickupDate" id="pickupDate"/>
+							</div>
+							<br>
+							<div class="info">
+								<label class="sort">Return Date:</label><br>
+								<input type="date" class="form-control" name="returnDate" id="returnDate"/>
+							</div>
+							<div><button class="btn3" type="button" onclick="calculatePrice(<?= $Price_per_day ?>)"><span>Calculate Total Price</span></button>
+								<label class="sort">Total Price:</label>
+								<p class="ans"><span style="color:green">$</span><p class="ans" id="output"></p></p>
+							</div>
+								<button class="btn1" name=reserve type="submit" disabled><span>Reserve</span></button>
+							</form>
+							<form method="get" name="form" action="">
+								<button class="btn1" name="plate2" type="submit" disabled><span> Pay </span></button>
+							</form>
 						</div>
-						<div><button class="btn3" type="button" onclick="calculatePrice(<?= $Price_per_day ?>)"><span>Calculate Total Price</span></button><br>
-							<label class="sort">Total Price:</label><br>
-							<p class="ans">$<p class="ans" id="output"></p></p>
-						</div>
-							<button class="btn1" name=reserve type="submit" disabled><span>Reserve</span></button>
-						</form>
-						<form method="get" name="form" action="checkout.php">
-							<button class="btn" name="plate2" type="submit" value=<?= $plateid ?>><span> Pay </span></button>
-						</form>
 						<?php
 					}
 					else
 					{
 						?>
-						<form method='POST' action='' onsubmit="return validateForm()">
-						<div class="info">
-							<label class="sort">Pickup Date:</label><br>
-							<input type="date" class="form-control" name="pickupDate" id="pickupDate"/>
+						<div class="formDate">
+							<form method='POST' action="" onsubmit="return validateForm()">
+							<div class="info">
+								<label class="sort">Pickup Date:</label><br>
+								<input type="date" class="form-control" name="pickupDate" id="pickupDate"/>
+							</div>
+							<br>
+							<div class="info">
+								<label class="sort">Return Date:</label><br>
+								<input type="date" class="form-control" name="returnDate" id="returnDate"/>
+							</div>
+							<div><button class="btn3" type="button" onclick="calculatePrice(<?= $Price_per_day ?>)"><span>Calculate Total Price</span></button><br>
+								<label class="sort">Total Price:</label><br>
+								<p class="ans"><span style="color:green">$</span><p class="ans" id="output"></p></p>
+							</div>
+								<button class="btn2" name=reserve type="submit" ><span>Reserve</span></button>
+							</form>
+							<form method="get" name="form" action="">
+								<button class="btn1" name="plate2" type="submit" disabled><span> Pay </span></button>
+							</form>
 						</div>
-						<br>
-						<div class="info">
-							<label class="sort">Return Date:</label><br>
-							<input type="date" class="form-control" name="returnDate" id="returnDate"/>
-						</div>
-						<div><button class="btn3" type="button" onclick="calculatePrice(<?= $Price_per_day ?>)"><span>Calculate Total Price</span></button><br>
-							<label class="sort">Total Price:</label><br>
-							<p class="ans">$<p class="ans" id="output"></p></p>
-						</div>
-							<button class="btn1" name=reserve type="submit" disabled><span>Reserve</span></button>
-						</form>
-						<form method="get" name="form" action="checkout.php">
-							<button class="btn1" name="plate2" type="submit" disabled><span> Pay </span></button>
-						</form>
 						<?php
 					}
+					
 				}
-				elseif($checkStatus == 'Pending')
+				elseif($checkStatus=='Rented')
 				{
 					?>
-						<form method='POST' action="" onsubmit="return validateForm()">
-						<div class="info">
-							<label class="sort">Pickup Date:</label><br>
-							<input type="date" class="form-control" name="pickupDate" id="pickupDate"/>
-						</div>
-						<br>
-						<div class="info">
-							<label class="sort">Return Date:</label><br>
-							<input type="date" class="form-control" name="returnDate" id="returnDate"/>
-						</div>
-						<div><button class="btn3" type="button" onclick="calculatePrice(<?= $Price_per_day ?>)"><span>Calculate Total Price</span></button><br>
-							<label class="sort">Total Price:</label><br>
-							<p class="ans">$<p class="ans" id="output"></p></p>
-						</div>
-							<button class="btn1" name=reserve type="submit" disabled><span>Reserve</span></button>
-						</form>
-						<form method="get" name="form" action="">
-							<button class="btn1" name="plate2" type="submit" disabled><span> Pay </span></button>
-						</form>
+						<button class="btn1" name=reserve type="submit"><span>Reserve</span></button>
+						<button class="btn1" name="plate2" type="submit" disabled><span> Pay </span></button>
 					<?php
 				}
-				else
+				elseif($checkStatus=='OutOfService')
 				{
 					?>
-						<form method='POST' action="" onsubmit="return validateForm()">
-						<div class="info">
-							<label class="sort">Pickup Date:</label><br>
-							<input type="date" class="form-control" name="pickupDate" id="pickupDate"/>
-						</div>
-						<br>
-						<div class="info">
-							<label class="sort">Return Date:</label><br>
-							<input type="date" class="form-control" name="returnDate" id="returnDate"/>
-						</div>
-						<div><button class="btn3" type="button" onclick="calculatePrice(<?= $Price_per_day ?>)"><span>Calculate Total Price</span></button><br>
-							<label class="sort">Total Price:</label><br>
-							<p class="ans">$<p class="ans" id="output"></p></p>
-						</div>
-							<button class="btn2" name=reserve type="submit" ><span>Reserve</span></button>
-						</form>
-						<form method="get" name="form" action="">
-							<button class="btn1" name="plate2" type="submit" disabled><span> Pay </span></button>
-						</form>
+						<button class="btn1" name=reserve type="submit" disabled><span>Reserve</span></button>
+						<button class="btn1" name="plate2" type="submit" disabled><span> Pay </span></button>
 					<?php
 				}
-				
-			}
-			elseif($checkStatus=='Rented')
-			{
-				?>
-					<button class="btn1" name=reserve type="submit"><span>Reserve</span></button>
-					<button class="btn1" name="plate2" type="submit" disabled><span> Pay </span></button>
-				<?php
-			}
-			elseif($checkStatus=='OutOfService')
-			{
-				?>
-					<button class="btn1" name=reserve type="submit" disabled><span>Reserve</span></button>
-					<button class="btn1" name="plate2" type="submit" disabled><span> Pay </span></button>
-				<?php
-			}
-		?>
-
-	</div>
+			?>
 	
+		</div>
+	</section>
 	<?php
 		if(isset($_POST['reserve']))
 		{
